5f0d2fd3eff272198cd94980e0c58add0497bc07
ralcus <ralcus.com@gmail.com>
2014-07-03 23:02:39 +0100

    Using chaining arrows in the apt-get-repositories file.

--- modules/apt-get-repositories/manifests/init.pp
+++ modules/apt-get-repositories/manifests/init.pp
@@ -4,14 +4,12 @@ class apt-get-repositories
         ensure => present,
         require => [Exec['initial apt-get update']]
     }
-
+    ->
     exec { 'git-ppa':
         command => '/usr/bin/add-apt-repository ppa:git-core/ppa -y',
-        require => [Package['python-software-properties']],
     }
-
+    ->
     exec { "ppa-apt-update":
         command => 'apt-get update',
-        require => [Exec['git-ppa']]
     }
 }
