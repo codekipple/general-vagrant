aa19a6f7671e16d04112db718d2e04f7d6bdab4b
ralcus <ralcus.com@gmail.com>
2014-08-17 15:08:12 +0100

    Added directory ensure for missbolt site.

--- manifests/sites/missbolt.pp
+++ manifests/sites/missbolt.pp
@@ -1,3 +1,10 @@
+file { "/var/www/missbolt" :
+    ensure => directory,
+    group => "vagrant",
+    owner => "vagrant",
+    recurse => true,
+}
+
 apache::vhost { 'missbolt.dev.com':
     port    => '80',
     docroot => '/var/www/missbolt/web',
