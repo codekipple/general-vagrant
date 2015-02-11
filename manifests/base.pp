3296532fc65e972d59d2dbb8f3d2ef37203bc294
ralcus <ralcus.com@gmail.com>
2015-02-11 18:06:57 +0000

    Made use of acts to get the ipaddress and netmast of eth1.

--- manifests/base.pp
+++ manifests/base.pp
@@ -6,7 +6,8 @@ file { '/etc/motd':
 content => "
   General dev VM
     - OS: Ubuntu 12.04
-    - IP: ${ip}
+    - IP: ${::ipaddress_eth1}
+    - Netmask: ${::netmask_eth1}
     - Git
     - Node
     - Ruby: 2.1.2 (using rbenv)
