cc7a9363b6536c0d4f5ea12bf705f0eac4d537cd
ralcus <ralcus.com@gmail.com>
2015-03-18 10:38:12 +0000

    No need to write to the exports file for nfs sharing as we arr now using vagrant synced folders.

--- manifests/nodes/nfs.pp
+++ manifests/nodes/nfs.pp
@@ -1,15 +1,15 @@
-# enable nfs sharing
+# # enable nfs sharing
 package { 'nfs-kernel-server':
     ensure => installed
 }
 
-file { '/etc/exports':
-    ensure => present,
-    owner => root, group => root,
-    content => template('/vagrant/manifests/nodes/templates/nfs/exports.erb'),
-    notify => Service['nfs-kernel-server'], # ssh will restart whenever you edit this file.
-    require => Package['nfs-kernel-server']
-}
+# file { '/etc/exports':
+#     ensure => present,
+#     owner => root, group => root,
+#     content => template('/vagrant/manifests/nodes/templates/nfs/exports.erb'),
+#     notify => Service['nfs-kernel-server'], # ssh will restart whenever you edit this file.
+#     require => Package['nfs-kernel-server']
+# }
 
 service { 'nfs-kernel-server':
     ensure => running,
