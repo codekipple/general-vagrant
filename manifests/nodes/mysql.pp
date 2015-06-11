2a3b9d04ba325e80905f0521e4b076c9bd6b2d68
ralcus <ralcus.com@gmail.com>
2015-06-11 09:50:39 +0100

    Tuned mysel settings a bit.

--- manifests/nodes/mysql.pp
+++ manifests/nodes/mysql.pp
@@ -1,3 +1,12 @@
 class { '::mysql::server':
     root_password => hiera('mysql::password')
 }
+
+file
+{
+    "/etc/mysql/my.cnf":
+        ensure  => present,
+        owner   => root, group => root,
+        notify  => Service['mysql'],
+        source => '/vagrant/manifests/nodes/templates/mysql/my.cnf',
+}
