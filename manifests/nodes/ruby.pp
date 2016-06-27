af48437543e0cbe06c8dbe4ab1e3243bf1ddfe5c
codekipple <codekipple@gmail.com>
2016-06-27 11:53:09 +0100

    Changes to ruby installation

--- manifests/nodes/ruby.pp
+++ manifests/nodes/ruby.pp
@@ -1,5 +1,8 @@
+# http://stackoverflow.com/questions/26595620/how-to-install-ruby-2-1-4-on-ubuntu-14-04
+
 rbenv::install { 'vagrant':
-    group => 'vagrant'
+    group => 'vagrant',
+    home => '/home/vagrant'
 }
 rbenv::compile { '2.1.2':
     user => 'vagrant',
